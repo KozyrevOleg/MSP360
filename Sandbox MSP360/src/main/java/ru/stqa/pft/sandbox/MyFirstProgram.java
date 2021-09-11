14a903e4c66e8390a38dbc30efffc5f5ed640184
KozyrevOleg <o.kozyrev@inbox.ru>
2021-09-11 23:55:29 +0300

    Добавлено несколько функций

--- Sandbox MSP360/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ Sandbox MSP360/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,6 +3,27 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 	
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        hello("world");
+        hello("Anna");
+        hello("Oleg");
+
+        double l= 5;
+        System.out.println("Площадь квадрата sо sтороной " + l + " = " + area (l));
+
+        double a = 4;
+        double b = 5;
+        System.out.println("Площадь прмоугольника so тороной " + a + " и " + b + " = " + area (a,b));
+    }
+
+    public static void hello(String somebody) {
+        System.out.println("Hello, " + somebody + "!");
+    }
+
+    public static double area (double len) {
+        return len*len;
+    }
+
+    public static double area (double a, double b) {
+        return a * b;
     }
 }
